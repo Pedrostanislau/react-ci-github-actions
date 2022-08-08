71e7a58a001425966fcdf2d1b7f36f3b5025c69f
Pedro Stanislau Tomacheski - PTM <pedro.tomacheski@iteris.com.br>
2022-08-08 10:37:55 -0300

    Adding infra icons to footer

--- src/components/Section/Footer/Footer.jsx
+++ src/components/Section/Footer/Footer.jsx
@@ -7,7 +7,12 @@ import {
   FaInstagram,
   FaGithub,
   FaTwitter,
+  FaGoogle,
 } from "react-icons/fa";
+import AWSIcon from 'react-aws-icons/dist/aws/logo/AWS';
+import S3 from 'react-aws-icons/dist/aws/logo/S3';
+import CloudFront from 'react-aws-icons/dist/aws/logo/CloudFront';
+import Route53 from 'react-aws-icons/dist/aws/logo/Route53';
 import { SiLinktree } from "react-icons/si";
 import { IoMdRocket } from "react-icons/io";
 
@@ -56,6 +61,28 @@ const Footer = () => {
         </a>
         <a href={"https://linktr.ee/pstanislau"} target="_blank" rel="noreferrer">
           <SiLinktree />
+        </a>
+      </div>
+
+      <div className="social_media">
+      <a href={"https://aws.amazon.com"} target="_blank" rel="noreferrer">
+          <AWSIcon/>
+        </a>
+
+        <a href={"https://aws.amazon.com/s3/"} target="_blank" rel="noreferrer">
+          <S3/>
+        </a>
+
+        <a href={"https://aws.amazon.com/pt/cloudfront/"} target="_blank" rel="noreferrer">
+          <CloudFront/>
+        </a>
+
+        <a href={"https://aws.amazon.com/pt/route53/"} target="_blank" rel="noreferrer">
+          <Route53/>
+        </a>
+
+        <a href={"https://domains.google/"} target="_blank" rel="noreferrer">
+          <FaGoogle/>
         </a>
       </div>
       <div className="rocket_container">
