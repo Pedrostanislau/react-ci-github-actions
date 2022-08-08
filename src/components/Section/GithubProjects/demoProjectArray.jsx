737d0aa6ed5db151a712ca5c0b71e67c7a201992
Pedro Stanislau Tomacheski - PTM <pedro.tomacheski@iteris.com.br>
2022-08-08 03:34:28 -0300

    Adding projects

--- src/components/Section/GithubProjects/demoProjectArray.jsx
+++ src/components/Section/GithubProjects/demoProjectArray.jsx
@@ -1,8 +1,8 @@
 import img1 from "../../../assets/githubRepo/Img10.jpg";
-import img2 from "../../../assets/githubRepo/Img20.png";
-import img3 from "../../../assets/githubRepo/Img3.jpg";
-import img4 from "../../../assets/githubRepo/Img4.jfif";
-import img5 from "../../../assets/githubRepo/Img5.jfif";
+import img2 from "../../../assets/githubRepo/Img20.jfif";
+import img3 from "../../../assets/githubRepo/Img30.png";
+import img4 from "../../../assets/githubRepo/Img40.jpg";
+import img5 from "../../../assets/githubRepo/Img50.jpg";
 import img6 from "../../../assets/githubRepo/Img6.jfif";
 
 
@@ -17,36 +17,36 @@ export const data = [
   {
     id: 2,
     image: img2,
-    title: "Telegram Bot",
-    github: "",
+    title: "Catching Waves",
+    github: "https://github.com/pstanislau/WAVE",
     demo: "",
   },
   {
     id: 3,
     image: img3,
-    title: "Twitter Bot",
-    github: "https://github.com/losier/TwitterBot",
-    demo: "https://losier.github.io/",
+    title: "MQTT Reply",
+    github: "https://github.com/pstanislau/MQTT",
+    demo: "",
   },
   {
     id: 4,
     image: img4,
-    title: "Discord Bot",
-    github: "https://github.com/losier/Kiri",
-    demo: "https://discord.com/api/oauth2/authorize?client_id=900390993860366356&permissions=8&scope=bot",
+    title: "Pet Adoption",
+    github: "https://github.com/pstanislau/adocao",
+    demo: "",
   },
   {
     id: 5,
     image: img5,
-    title: "Dark Chrome",
-    github: "https://github.com/losier/Dark-Chrome",
-    demo: "https://github.com/losier/Dark-Chrome/blob/master/Src/DarkChrome.exe",
+    title: "Hackathon",
+    github: "https://github.com/decola-time05/hackathon-decola",
+    demo: "",
   },
   {
     id: 6,
     image: img6,
     title: "ToDo",
-    github: "https://github.com/losier/Todo",
-    demo: "https://losier.github.io/Todo",
+    github: "https://github.com/pstanislau/ToDo",
+    demo: "",
   },
 ];
